d3c45b866d6d0e4b577cac1dc80855a8ab9bc786
Bettiol <e-fibo@gft.com>
2023-11-17 12:55:51 +0000

    updating message

--- kafka-consumer/src/main/java/com/userregistration/kafkaconsumer/ConsumerService.java
+++ kafka-consumer/src/main/java/com/userregistration/kafkaconsumer/ConsumerService.java
@@ -13,7 +13,14 @@ public class ConsumerService {
 
     @KafkaListener(topics = Constants.USER_REG_TOPIC, groupId = "reg", containerFactory = "userRegListener")
     public void consume(UserRegistrationEvent usrReg) {
-        log.info(String.format("Consumed User reg event -> %s", usrReg.toString()));
+        final StringBuilder logMsg = new StringBuilder();
+        logMsg.append("\n----- >Consumed User reg event <-----\n");
+        logMsg.append(String.format("Id: %s\n", usrReg.getUserId()));
+        logMsg.append(String.format("UserName: %s\n", usrReg.getUsername()));
+        logMsg.append(String.format("Email: %s\n", usrReg.getEmail()));
+        logMsg.append(String.format("Date: %s\n", usrReg.getRegistrationDate().toString()));
+        logMsg.append("\n\n");
+        log.info(logMsg.toString());
     }
 }
 
